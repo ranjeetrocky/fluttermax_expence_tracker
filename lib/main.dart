09bab8b62d8637a72ff81fea269931420c4b35cd
ranjeetrocky <ranjeetrocky2000@gmail.com>
2022-02-17 20:17:57 +0530

    added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:max2_expence_tracker/widgets/new_transaction.dart';
 import 'package:max2_expence_tracker/widgets/transaction_list.dart';
-import './widgets/user_transactions.dart';
 import 'models/transaction.dart';
 
 void main() {
@@ -19,14 +18,28 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
+        fontFamily: 'QuickSand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline5: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold),
+        ),
+        // fontFamily: 'OpenSans',
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -34,18 +47,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactionList = [
-    Transaction(
-      id: "t1",
-      title: "Soup",
-      amount: 200,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: "t1",
-      title: "Soup",
-      amount: 200,
-      date: DateTime.now(),
-    ),
     // Transaction(
     //   id: "t1",
     //   title: "Soup",
@@ -58,60 +59,6 @@ class _MyHomePageState extends State<MyHomePage> {
     //   amount: 200,
     //   date: DateTime.now(),
     // ),
-    // Transaction(
-    //   id: "t1",
-    //   title: "Soup",
-    //   amount: 200,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t1",
-    //   title: "Soup",
-    //   amount: 200,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t1",
-    //   title: "Soup",
-    //   amount: 200,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t1",
-    //   title: "Soup",
-    //   amount: 200,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t1",
-    //   title: "Soup",
-    //   amount: 200,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t1",
-    //   title: "Soup",
-    //   amount: 200,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t1",
-    //   title: "Soup",
-    //   amount: 200,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t1",
-    //   title: "Soup",
-    //   amount: 200,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: "t1",
-    //   title: "Soup",
-    //   amount: 200,
-    //   date: DateTime.now(),
-    // )
   ];
 
   void _addNewTransaction({required String title, required double amount}) {
@@ -137,22 +84,26 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Personal Expenses"),
+        title: const Text(
+          "Personal Expenses",
+          // style: TextStyle(fontFamily: "OpenSans"),
+        ),
         actions: [
           IconButton(
-              onPressed: () => _showAddModal(context), icon: Icon(Icons.add))
+              onPressed: () => _showAddModal(context),
+              icon: const Icon(Icons.add))
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _showAddModal(context),
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
             Card(
               child: Container(
-                child: Text("Chart!"),
+                child: const Text("Chart!"),
                 width: double.infinity,
               ),
               elevation: 5,
